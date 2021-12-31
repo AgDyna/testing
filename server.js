a7176ce28290535c7b3994928091e62bc35e9dff
AgDyna <TEAM@agdyna.com>
2021-12-31 15:09:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mysql from 'mysql'
+
+const port = 4000
+
+const app = express()
+// mySql DB
+const connection = mysql.createConnection({
+  host: process.env.Host || 'localhost',
+  user: process.env.User || 'root',
+  password: process.env.Password || '',
+  database: process.env.Database || 'farmsured_php_testing',
+})
+
+// DB connection
+connection.connect((err) => {
+  if (err) {
+    console.log(err)
+    throw err
+  }
+  console.log('MySql Database Connected ')
+})
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server at http://127.0.0.1:${port}`)
+})
